99ffe918e3cf1a82222a62415b71af7c6f90abe3
trantienthuong <thuongtt@7saturday.com>
2020-04-29 08:46:16 +0700

    access token Jwt-passport

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,26 +2,32 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
-    private userRepository: UserRepository
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) { }
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return await this.userRepository.signUp(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto){
-    // const result= await this.userRepository.validationUserPassword(authCredentialsDto);
-    const username = await this.userRepository.validateUserPassword(authCredentialsDto);
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
 
-    console.log('username', username);
+    const username = await this.userRepository.validateUserPassword(authCredentialsDto);
+    // console.log('username', username);
     if (!username) {
       throw new UnauthorizedException('Invalid credentials');
     }
+    const payload: JwtPayload = { username };
+    const accessToken = await this.jwtService.sign(payload);
+
+    return { accessToken };
 
   }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -28,8 +28,6 @@ export class UserRepository extends Repository<User>{
     }
   }
 
-
-
   async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
     const { username, password } = authCredentialsDto;
     const user = await this.findOne({ username });
